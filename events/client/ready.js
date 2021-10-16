7f8f93e9b229fdbae8cbb148304f087469382809
ZartaX0O3 <arthur.avd69@gmail.com>
2021-10-16 19:57:22 +0200

    -- V4 --

--- events/client/ready.js
+++ events/client/ready.js
@@ -1,6 +1,4 @@
 module.exports = client => {
-    //SETTING ALL GUILD DATA FOR THE DJ ONLY COMMANDS for the DEFAULT
-    //client.guilds.cache.forEach(guild=>client.settings.set(guild.id, ["autoplay", "clearqueue", "forward", "loop", "jump", "loopqueue", "loopsong", "move", "pause", "resume", "removetrack", "removedupe", "restart", "rewind", "seek", "shuffle", "skip", "stop", "volume"], "djonlycmds"))
     try{
         try{
             const stringlength = 69;
