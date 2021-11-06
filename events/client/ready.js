8f903fa9cbc61624a7b097b58102be229d9476cb
ZartaX0O3 <arthur.avd69@gmail.com>
2021-11-06 12:12:25 +0100

    Modification ready.js

--- events/client/ready.js
+++ events/client/ready.js
@@ -1,4 +1,7 @@
-module.exports = client => {
+const {MessageEmbed} = require("discord.js");
+const emote = require("../../emojis.json");
+const channels = require("../../channels.json");
+module.exports = (client,message) => {
     try{
         try{
             const stringlength = 69;
@@ -9,6 +12,12 @@ module.exports = client => {
             console.log(`     ┃ ` + ` /--/ ${client.user.tag} /--/ `+ " ".repeat(-1+stringlength-` ┃ `.length-` /--/ ${client.user.tag} /--/ `.length)+ "┃")
             console.log(`     ┃ ` + " ".repeat(-1+stringlength-` ┃ `.length)+ "┃")
             console.log(`     ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛`)
+
+            const embedReady = new MessageEmbed()
+                .setColor("ffa600")
+                .setTitle(`${emote.emojis.orange_circle} **Allumage du BOT v2**`)
+
+            message.guild.channels.cache.get(channels.sanctionLogsChannel).send({embeds: [embedReady]})
         }catch{}
 
     } catch (e){
